146d6299da4130440ac4b25bcc920896762f4506
Avinash Kumar <avi8654340@gmail.com>
2022-06-26 06:17:19 +0530

    again same

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x&&&07$i^i1j^@ah4cgq@^(&dth(c^xne0cge0c9+e&atgc8k_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -120,14 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/backend/static'
-MEDIA_URL = '/backend/static/images'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'backend/static'
+    BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT = BASE_DIR / 'backend/static/images'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 
 
