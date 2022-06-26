433ebb9c0ad4f6c505178c36d4e2559efd13526c
Avinash Kumar <avi8654340@gmail.com>
2022-06-26 06:36:45 +0530

    staticfiles configured

--- backend/settings.py
+++ backend/settings.py
@@ -120,12 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
